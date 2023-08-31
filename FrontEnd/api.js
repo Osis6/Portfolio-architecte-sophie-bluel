d504ddab882e9e318406aac0766d7af54dbd1a02
BOUNOU <djeey.osis@gmail.com>
2023-09-01 00:46:10 +0200

    Réalisation du filtre des travaux, La possibilité de filtrer la galerie par catégorie de projet.

--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -1,4 +1,5 @@
 const BASE_API_URL = "http://localhost:5678/api";
+let selectedCategory = null; // Aucune categorie selectionner
 
 async function getWorks() {
   const response = await fetch(`${BASE_API_URL}/works`);
@@ -9,16 +10,60 @@ async function getWorks() {
 async function displayWorks() {
   const works = await getWorks();
   const galleryDiv = document.querySelector(".gallery");
+  galleryDiv.innerHTML = "";
 
-  works.forEach((work) => {
-    const workElement = document.createElement("div");
-    workElement.classList.add("work-item");
-    workElement.innerHTML = `
-      <img src="${work.imageUrl}">
-      <p>${work.title}</p>
-    `;
-    galleryDiv.appendChild(workElement);
-  });
+  works
+    .filter(
+      (work) =>
+        selectedCategory === null ||
+        selectedCategory === "Tous" ||
+        work.categoryId === selectedCategory
+    )
+    .forEach((work) => {
+      const workElement = document.createElement("div");
+      workElement.classList.add("work-item");
+      workElement.innerHTML = `
+          <img src="${work.imageUrl}">
+          <p>${work.title}</p>
+        `;
+      galleryDiv.appendChild(workElement);
+    });
 }
 
-displayWorks();
+async function getCategories() {
+  const response = await fetch(`${BASE_API_URL}/categories`);
+  const categories = await response.json();
+  return categories;
+}
+
+async function displayCategories() {
+  const categories = await getCategories();
+  const filtersDiv = document.createElement("div");
+  filtersDiv.classList.add("filters");
+
+  const filterButtonAll = document.createElement("button");
+  filterButtonAll.textContent = "Tous";
+  filterButtonAll.addEventListener("click", async () => {
+    selectedCategory = "Tous";
+    await displayWorks();
+  });
+  filtersDiv.appendChild(filterButtonAll);
+
+  categories.forEach((category) => {
+    const filterButton = document.createElement("button");
+    filterButton.textContent = category.name;
+    filterButton.addEventListener("click", async () => {
+      selectedCategory = category.id;
+      await displayWorks();
+    });
+    filtersDiv.appendChild(filterButton);
+  });
+  const portfolioSection = document.getElementById("portfolio");
+  const galleryDiv = portfolioSection.querySelector(".gallery");
+  portfolioSection.insertBefore(filtersDiv, galleryDiv);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await displayWorks();
+  await displayCategories();
+});
