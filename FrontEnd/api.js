9f311f9b2cda49bba6fb5b23ed384a5328cea832
BOUNOU <djeey.osis@gmail.com>
2023-09-05 10:36:54 +0200

    Le code refait à neuf le fichier api.js dois uniquement contenir les appels API

--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -1,69 +1,21 @@
-const BASE_API_URL = "http://localhost:5678/api";
-let selectedCategory = null; // Aucune categorie selectionner
+// api.js : Ce fichier contient les appels API
 
+// Définition de la constante BASE_API_URL
+const BASE_API_URL = "http://localhost:5678/api";
+
+// Fonction pour obtenir les œuvres depuis l'API
 async function getWorks() {
   const response = await fetch(`${BASE_API_URL}/works`);
   const dataWorks = await response.json();
   return dataWorks;
 }
 
-async function displayWorks() {
-  const works = await getWorks();
-  const galleryDiv = document.querySelector(".gallery");
-  galleryDiv.innerHTML = "";
-
-  works
-    .filter(
-      (work) =>
-        selectedCategory === null ||
-        selectedCategory === "Tous" ||
-        work.categoryId === selectedCategory
-    )
-    .forEach((work) => {
-      const workElement = document.createElement("div");
-      workElement.classList.add("work-item");
-      workElement.innerHTML = `
-          <img src="${work.imageUrl}">
-          <p>${work.title}</p>
-        `;
-      galleryDiv.appendChild(workElement);
-    });
-}
-
+// Fonction pour obtenir les catégories depuis l'API
 async function getCategories() {
   const response = await fetch(`${BASE_API_URL}/categories`);
   const categories = await response.json();
   return categories;
 }
 
-async function displayCategories() {
-  const categories = await getCategories();
-  const filtersDiv = document.createElement("div");
-  filtersDiv.classList.add("filters");
-
-  const filterButtonAll = document.createElement("button");
-  filterButtonAll.textContent = "Tous";
-  filterButtonAll.addEventListener("click", async () => {
-    selectedCategory = "Tous";
-    await displayWorks();
-  });
-  filtersDiv.appendChild(filterButtonAll);
-
-  categories.forEach((category) => {
-    const filterButton = document.createElement("button");
-    filterButton.textContent = category.name;
-    filterButton.addEventListener("click", async () => {
-      selectedCategory = category.id;
-      await displayWorks();
-    });
-    filtersDiv.appendChild(filterButton);
-  });
-  const portfolioSection = document.getElementById("portfolio");
-  const galleryDiv = portfolioSection.querySelector(".gallery");
-  portfolioSection.insertBefore(filtersDiv, galleryDiv);
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
-  await displayWorks();
-  await displayCategories();
-});
+// Export des fonctions et de la constante
+export { BASE_API_URL, getWorks, getCategories };
---
+++ FrontEnd/index.js
@@ -0,0 +1,75 @@
+// index.js : Ce fichier gère l'affichage des œuvres et des filtres par catégorie et la modal
+
+// Importez les fonctions et la constante depuis api.js
+import { BASE_API_URL, getWorks, getCategories } from "./api.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Code pour afficher les œuvres et gérer les filtres par catégorie
+  let selectedCategory = null; // Initialement aucune catégorie sélectionnée
+
+  async function displayWorks() {
+    // Récupérer la liste des œuvres depuis l'API
+    const works = await getWorks();
+    const galleryDiv = document.querySelector(".gallery");
+    // Vider le contenu actuel de la galerie
+    galleryDiv.innerHTML = "";
+
+    // Filtrer les œuvres en fonction de la catégorie sélectionnée ou afficher toutes les œuvres
+    works
+      .filter(
+        (work) =>
+          selectedCategory === null ||
+          selectedCategory === "Tous" ||
+          work.categoryId === selectedCategory
+      )
+      .forEach((work) => {
+        const workElement = document.createElement("div");
+        workElement.classList.add("work-item");
+        workElement.innerHTML = `
+          <img src="${work.imageUrl}">
+          <p>${work.title}</p>
+        `;
+        // Ajouter l'élément à la galerie
+        galleryDiv.appendChild(workElement);
+      });
+  }
+
+  async function displayCategories() {
+    // Récupérer la liste des catégories depuis l'API
+    const categories = await getCategories();
+    const filtersDiv = document.createElement("div");
+    filtersDiv.classList.add("filters");
+
+    // Créer un bouton "Tous" pour afficher toutes les œuvres
+    const filterButtonAll = document.createElement("button");
+    filterButtonAll.textContent = "Tous";
+    filterButtonAll.addEventListener("click", async () => {
+      selectedCategory = "Tous";
+      await displayWorks();
+    });
+    filtersDiv.appendChild(filterButtonAll);
+
+    // Parcourir chaque catégorie et créer un bouton pour chaque catégorie
+    categories.forEach((category) => {
+      const filterButton = document.createElement("button");
+      filterButton.textContent = category.name;
+      filterButton.addEventListener("click", async () => {
+        selectedCategory = category.id;
+        await displayWorks();
+      });
+      filtersDiv.appendChild(filterButton);
+    });
+
+    // Renvoyer l'élément div contenant les boutons de filtre de catégorie
+    return filtersDiv;
+  }
+  async function setupPage() {
+    const filtersDiv = await displayCategories();
+    // Attacher filtersDiv à la section du portfolio
+    const portfolioSection = document.getElementById("portfolio");
+    const galleryDiv = portfolioSection.querySelector(".gallery");
+    portfolioSection.insertBefore(filtersDiv, galleryDiv);
+  }
+  await setupPage();
+  await displayWorks();
+});
